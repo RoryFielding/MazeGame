e29cba77a402c6c0c778cfaca8b34eac03f3a92d
Rory Fielding <Rory-f@hotmail.co.uk>
2017-03-04 15:39:39 +0000

    Initial Commit

---
+++ Mazed-based Game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mazed-based Game
+//
+//  Created by Rory John Fielding on 04/03/2017.
+//  Copyright © 2017 Rory John Fielding. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
